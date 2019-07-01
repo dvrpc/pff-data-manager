7930280adc09beb1260582dbc20fdf818d2b25d4
Michael Ruane <mruane@dvrpc.org>
2019-07-01 10:48:45 -0400

    Update pports importer for directory access

--- pports-cargo-importer.py
+++ pports-cargo-importer.py
@@ -22,35 +22,56 @@ from settings import conn_string
 
 # check for necessary arguments before running
 if (len(sys.argv) < 3 or  len(sys.argv) > 3):
-       print """\
-       ERROR: This script requires two arguments:
+    print """\
+        ERROR: This script requires two arguments:
 
-       1 - type of import (m = multiple years or s = single year)
-       2 - source directory of files
+        1 - type of import (m = multiple years or s = single year)
+        2 - source directory of files
 
-       Usage: pports-cargo-importer.py <type> <directory>
-       """
-       sys.exit(1)
+        Usage: pports-cargo-importer.py <type> <directory>
+        """
+    sys.exit(1)
 
 #accept arguments from command line
 sourceType = sys.argv[1]
-location = sys.argv[2]
+path = sys.argv[2]
+
+# SQL statement for the two tables
+SQL_INSERT_CARGO = """
+INSERT STATEMENT
+"""
+SQL_INSERT_TRIPS= """
+INSERT STATEMENT
+"""
 
 # parse the files
-# def parseFiles():
+def parseAnnualData(annual_dir, year):
+    for root, dirs, files in os.walk( annual_dir ):
+        for file in files:
+            dataType = os.path.basename(os.path.normpath( root ))
+            
+            ## DO SOMETHING WITH THE DATA
+            #  file -> csv      [os.path.join(root, file)  <---  access the file with full path] 
+            #  dataType -> cargo or trips (pulled from the directory name)
+    return
 
+# check type of request and execute data parsing
+if sourceType == 'm':
+    # loop all multiple year directories 
+    dirs = os.listdir( path )
+    for d in dirs:
+        parseAnnualData(os.path.join(path, d), d)           
+elif sourceType == 's':
+    d = os.path.basename(os.path.normpath( path ))
+    parseAnnualData(path, d)
+else:
+    print """\
+        ERROR: This script requires two arguments:
 
+        1 - type of import (m = multiple years or s = single year)
+        2 - source directory of files
 
-
-# if sourceType == 'm':
-#        print 'Importing Principal Ports data for multiple years...'
-#        # loop all files in specified directory
-#        for file in os.listdir(location):
-
-
-# elif sourceType == 's':
-#        print 'Importing Principal Ports data for a single year...'
-
-con = psql.connect(conn_string)
-cur = con.cursor()
+        Usage: pports-cargo-importer.py <type> <directory>
+        """
+    sys.exit(1)
 
