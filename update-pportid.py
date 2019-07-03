adff73a77234d8df7b77129fb02da9de72e36c50
Michael Ruane <mruane@dvrpc.org>
2019-07-03 10:42:26 -0400

    Fix bad fed data
    Updates all records in `pports_act` to have full 5 character port ids

---
+++ update-pportid.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+##################################
+#
+#   Update WCSC data to correct for 4 character port ids missing leading zero
+#   Author: Michael Ruane, DVRPC
+#   Date: June 28, 2019
+#
+#################################
+
+import os
+import sys
+import psycopg2 as psql
+from settings import conn_string
+
+
+# SQL statement
+SQL_QUERY = """
+SELECT oid, portid FROM pports_act
+WHERE length(portid)<5 
+"""
+SQL_UPDATE = """
+UPDATE pports_act
+SET portid = %s
+WHERE oid = %s
+"""
+con = psql.connect(conn_string)
+cur = con.cursor()
+
+cur.execute(SQL_QUERY)
+data = cur.fetchall()
+
+for line in data:
+    oid = line[0]
+    prefix = line[1][:1]
+    code = line[1][1:]
+    fullid = prefix + "0" + code
+    cur.execute(SQL_UPDATE, (fullid, oid))
+
+cur.close()
+con.commit()
+con.close()
